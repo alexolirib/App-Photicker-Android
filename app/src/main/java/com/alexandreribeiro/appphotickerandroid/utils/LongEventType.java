ea49ce40e1ee80c45b996d4ce8aaacc3dfcb2dc7
Alexandre Ribeiro <alexolirib@gmail.com>
2018-12-02 17:49:29 -0200

    criado thread para a ação fique contínuo

---
+++ app/src/main/java/com/alexandreribeiro/appphotickerandroid/utils/LongEventType.java
@@ -0,0 +1,5 @@
+package com.alexandreribeiro.appphotickerandroid.utils;
+
+public enum LongEventType {
+    ZoomIn, ZoomOut, RotateLeft, RotateRight
+}
--- app/src/main/java/com/alexandreribeiro/appphotickerandroid/views/MainActivity.java
+++ app/src/main/java/com/alexandreribeiro/appphotickerandroid/views/MainActivity.java
@@ -1,9 +1,10 @@
 package com.alexandreribeiro.appphotickerandroid.views;
 
 import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -11,12 +12,18 @@ import android.widget.RelativeLayout;
 
 import com.alexandreribeiro.appphotickerandroid.R;
 import com.alexandreribeiro.appphotickerandroid.utils.ImageUtil;
+import com.alexandreribeiro.appphotickerandroid.utils.LongEventType;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener, View.OnTouchListener{
+
+
     private final ViewHolderMain mViewHolderMain = new ViewHolderMain();
     private ImageView mImagemSelected;
+    private boolean mAutoIncrement;
+    private LongEventType mLongEventType;
+    private Handler mRepeatUpdateHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         List<Integer> mListImage = ImageUtil.getImageList();
-        Log.e("qtd", String.valueOf(mListImage.size()));
 
         mViewHolderMain.mRelativePhotoContent = findViewById(R.id.relative_photo_content_draw);
         final LinearLayout content = this.findViewById(R.id.linear_horizontal_scroll_content);
@@ -70,6 +76,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewHolderMain.mBtnRotateRight.setOnClickListener(this);
         this.mViewHolderMain.mBtnZoomIn.setOnClickListener(this);
         this.mViewHolderMain.mBtnZoomOut.setOnClickListener(this);
+
+        this.mViewHolderMain.mBtnRotateLeft.setOnLongClickListener(this);
+        this.mViewHolderMain.mBtnRotateRight.setOnLongClickListener(this);
+        this.mViewHolderMain.mBtnZoomIn.setOnLongClickListener(this);
+        this.mViewHolderMain.mBtnZoomOut.setOnLongClickListener(this);
+
+        this.mViewHolderMain.mBtnRotateLeft.setOnTouchListener(this);
+        this.mViewHolderMain.mBtnRotateRight.setOnTouchListener(this);
+        this.mViewHolderMain.mBtnZoomIn.setOnTouchListener(this);
+        this.mViewHolderMain.mBtnZoomOut.setOnTouchListener(this);
     }
 
     @Override
@@ -112,6 +128,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mImagemSelected = image;
 
                 toogleControlPanel(true);
+
+                //para conseguir movimentar a imagem com o dedo
+
+                image.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent motionEvent) {
+                        float x, y;
+                        switch (motionEvent.getAction()){
+                            //caso clique na imagem
+                            case MotionEvent.ACTION_DOWN:
+                                mImagemSelected = image;
+                                toogleControlPanel(true);
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                int coords[] = {0,0};
+                                //preenche o posicionamento x e y , sabendo assim o posicionamento dele na tela
+                                relativeLayout.getLocationOnScreen(coords);
+
+                                //calcular onde o usuario quer movimentar na tela
+                                // dive por 2 para o centro da imagem seja o centro dela
+                                x = (motionEvent.getRawX() - (image.getWidth()/2));
+                                //y é para saber o inicio do relative layout
+                                y = motionEvent.getRawY() - ((coords[1] + 100) + (image.getHeight()/2));
+                                image.setX(x);
+                                image.setY(y);
+                                break;
+                            //quando usuario parar de tocar na imagem
+                            case MotionEvent.ACTION_UP:
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
             }
         };
     }
@@ -128,6 +178,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    @Override
+    public boolean onLongClick(View v) {
+        int id = v.getId();
+        if(id == R.id.img_zoom_in){
+            this.mLongEventType = LongEventType.ZoomIn;
+        }
+        if(id == R.id.img_zoom_out){
+            this.mLongEventType = LongEventType.ZoomOut;
+        }
+        if(id == R.id.img_rotate_left){
+            this.mLongEventType = LongEventType.RotateLeft;
+        }
+        if(id == R.id.img_rotate_right){
+            this.mLongEventType = LongEventType.RotateRight;
+        }
+        //ele ativa quando começar o click
+        mAutoIncrement = true;
+        //aqui chamar a thread
+        new RptUpdater().run();
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent motionEvent) {
+        int id = v.getId();
+        if(id == R.id.img_zoom_in || id == R.id.img_zoom_out || id == R.id.img_rotate_right || id == R.id.img_rotate_left){
+            //parou de executar
+            //desativa quando tirar o dedo
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP && mAutoIncrement == true){
+                mAutoIncrement = false;
+                this.mLongEventType = null;
+            }
+        }
+        return false;
+    }
 
 
     private static  class  ViewHolderMain{
@@ -143,5 +229,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         RelativeLayout mRelativePhotoContent;
 
+    }
+
+    private class RptUpdater implements Runnable {
+
+        @Override
+        public void run() {
+            //por enquanto que for true vai continuar fazer a ação
+            if(mAutoIncrement){
+                //aqui estará sempre chamando a thred toda vez que for true(mAutoIncrement) cada 0,050 segundos
+                mRepeatUpdateHandler.postDelayed(new RptUpdater(), 50);
+            }
+
+            //saber o evento que usuario está fazendo
+            if(mLongEventType != null){
+                switch (mLongEventType){
+                    case ZoomIn:
+                        ImageUtil.handleZoonIn(mImagemSelected);
+                        break;
+                    case ZoomOut:
+                        ImageUtil.handleZoonOut(mImagemSelected);
+                        break;
+                    case RotateLeft:
+                        ImageUtil.handleRotateLeft(mImagemSelected);
+                        break;
+                    case RotateRight:
+                        ImageUtil.handleRotateRight(mImagemSelected);
+                        break;
+
+                }
+            }
+        }
     }
 }
